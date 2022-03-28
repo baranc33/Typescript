05150f326e5ad38203aa628771703b7525c92908
Hakan Baran Çakir <baranc3363@gmail.com>
2022-03-28 20:55:45 +0300

    functions

--- vierone-typescript-tutorial/src/typefonksyon.ts
+++ vierone-typescript-tutorial/src/typefonksyon.ts
@@ -1,5 +1,8 @@
 // geri dönüş tipli fonksyonlar
 
+import { rejects } from "assert";
+import { resolve } from "path";
+
 
 
 function Bir(): number {
@@ -13,3 +16,37 @@ function İki(): [string, number] {
 }
 
 let ad: [string ,number]=İki();
+
+
+// parametreli çalışma
+
+function NameAndLastName(firstName:string,lastname:string):string{
+    return firstName+" "+lastname;
+}
+
+
+let Name:string=NameAndLastName("Hakan Baran","Çakır");
+
+
+// bir  function yazcaz konuyla ilgili geriye promise dönen bir metot yazcaz
+// bu konuylşa ilgili detaylı bilgi vermedi hoca
+// https://medium.com/codefiction/javascriptte-promise-kullan%C4%B1m%C4%B1-ccca1123989a
+// konuyla ilgili araştırma yapılması gerekiyor yukardaki linkten
+
+async function LogPerson(isim:string,soyisim:string,yas:number):Promise<number> {
+    return new Promise((resolve,rejects)=>{
+        resolve(25);
+    });
+}
+
+// örnek bir function yazalım
+
+function yazmaca(isim:string,tekrar:number):void{
+
+    for(let i=0; i<tekrar;i++)
+    {
+        console.log(isim);
+    }
+
+
+}
