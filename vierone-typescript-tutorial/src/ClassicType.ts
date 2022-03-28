923b2ea7753484044204acd71ab850bd29572861
Hakan Baran Çakir <baranc3363@gmail.com>
2022-03-28 18:12:20 +0300

    classictype and typeof , instanceof

--- vierone-typescript-tutorial/src/ClassicType.ts
+++ vierone-typescript-tutorial/src/ClassicType.ts
@@ -8,4 +8,34 @@
 // undefined // belirsiz
 // Infinity// number bir tip
 
-const a =5;
+// type scriptle bir değişken oluşturunca otomatik tip verir
+// java scriptteki gibi let ve const kullanılabilir.
+
+let namess="Baran";
+// name=3;// bu hata verir çünkü önceden string tanımladık
+
+let myname:String="Hakan"; // bu şekilde tip verebiliriz
+
+// birden fazla tip verilebilir
+
+let mylastname:string | number="hakan";
+mylastname=2; // bu şekilde birden fazla tip desteği verilebilir.
+
+// type of kullanımı
+if(typeof mylastname==='string'){// 3 eşit js kullanımından gelmekte.
+    // burda string işlemlerini yapabilirz
+}
+
+
+// any tipi
+let b:any =45; // any ile tanımladığımız değişkenler tip desteği sağlamaz
+
+// instanceof   // type of gibi kullanılabilir 
+// anlamı bir değişken stringten instance almışsa kalıtım almıssa gibi bir anlam katar
+// bunu genelde html elemanları için kullanılır etikete direk müdehale edebilmek için
+
+
+if(b instanceof HTMLInputElement){
+    // burda İnput un özelliklerinie
+    // direk erişim sağliyabiliyoruz
+}
