6e498ecd7051efcdedad69ee05bd9db7a2faf8d1
Hakan Baran Çakir <baranc3363@gmail.com>
2022-03-28 20:31:14 +0300

    functionType

---
+++ vierone-typescript-tutorial/src/typefonksyon.ts
@@ -0,0 +1,15 @@
+// geri dönüş tipli fonksyonlar
+
+
+
+function Bir(): number {
+    return 1;
+}
+let  deger:Number=Bir()
+
+// birden fazla değer dönceği zaman tanımlama sekli
+function İki(): [string, number] {
+    return ["hakan", 26];
+}
+
+let ad: [string ,number]=İki();
